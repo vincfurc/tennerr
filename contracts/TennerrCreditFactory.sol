8eeb2369acc64bd7711ac8c11475c51790ce023c
Vince <10850139+vfurci200@users.noreply.github.com>
2021-06-22 17:12:01 +0100

    Credit token factory proof of concept

---
+++ contracts/TennerrCreditFactory.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./Tennerr.sol";
+
+
+
+contract TennerrCreditFactory is ERC20Burnable, AccessControl{
+  using SafeMath for uint;
+
+  // initiialize tennerr
+  Tennerr public tennerr;
+  address payable private _tennerrContractAddress;
+  // initiialize escrow
+  TennerrEscrow public tennerrEscrow;
+  address payable private _tennerrEscrowContractAddress;
+  // initiialize tennerr
+  TennerrController public tennerrController;
+  address payable private _tennerrControllerAddress;
+
+  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    // Create a new role identifier for the minter role
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+
+
+  constructor() public ERC20 ("Credit Tennerr Token","cTNR"){}
+
+
+  function mint(
+    uint amountDeposited,
+    string calldata currencyTicker)
+    external returns (uint256){
+      require(hasRole(MINTER_ROLE, msg.sender), "Caller is not an authorized minter");
+      require(amountDeposited>0, "Deposited amount is zero.");
+      uint256 exchangeRateToken = getCurrentExchangeRate(currencyTicker);
+      require(exchangeRateToken>0,"Exchange rate is zero");
+      tennerrController.depositToPool(TennerrController.LiquidityPool.Aave, currencyTicker, amountDeposited, msg.sender);
+      // either I get it from pool or compute it here privately based on minted
+      uint256 exchangeRatePool = tennerrController.getExchangeRate(TennerrController.LiquidityPool.Aave,currencyTicker);
+      require(exchangeRatePool>0,"Pool exchange rate is zero. Check pool gateway.");
+      uint256 mintedAmount = 0;
+      mintedAmount = amountDeposited.mul(10**27).div(exchangeRatePool).mul(exchangeRateToken).div(10**6);
+      require(mintedAmount>0,"Amount deposited not enough to mint tokens.");
+      _mint(msg.sender,mintedAmount);
+      return mintedAmount;
+  }
+
+  function getCurrentExchangeRate(string memory currencyTicker) internal returns (uint256){
+      return tennerrController.getExchangeRate(TennerrController.LiquidityPool.Aave,currencyTicker);
+  }
+
+  function setTennerr(address payable newContract) external {
+      require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+      _tennerrContractAddress = newContract;
+      tennerr = Tennerr(_tennerrContractAddress);
+  }
+
+  function setTennerrEscrow(address payable newContract) external {
+      require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+      _tennerrEscrowContractAddress = newContract;
+      tennerrEscrow = TennerrEscrow(_tennerrEscrowContractAddress);
+      grantRole(MINTER_ROLE, _tennerrEscrowContractAddress);
+  }
+
+}
