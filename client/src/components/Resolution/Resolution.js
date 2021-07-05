b14aefffee9a2fb3f0fa28643841f25b6254caf2
Christian Wilkie <christian.wilkie@gmail.com>
2021-07-05 12:29:09 -0400

    resolution tabs

--- client/src/components/Resolution/Resolution.js
+++ client/src/components/Resolution/Resolution.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Section, Tabs} from "react-bulma-components";
 
 export default function Resolution() {
+    const [disputeTabActive, setDisputeTabActive] = useState(true);
+
     return (
-        <div>
-            
-        </div>
+        <Tabs>
+            <Tabs.Tab className={disputeTabActive? 'is-active': ''} onClick={handleClickDisputeTab}>
+                My Disputes
+            </Tabs.Tab>
+            <Tabs.Tab className={!disputeTabActive? 'is-active': ''} onClick={handleClickResolutionTab}>
+                My Resolutions
+            </Tabs.Tab>
+        </Tabs>
     )
+
+    function handleClickDisputeTab() {
+        setDisputeTabActive(true);
+    }
+
+    function handleClickResolutionTab() {
+        setDisputeTabActive(false);
+    }
 }
 
